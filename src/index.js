2224ec2e632d2d76575666ffe64478fde0990791
Ayush Solanki <ayush.solanki@innovapptive.com>
2023-04-02 14:54:35 +0530

    env changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const { Amplify } = require("aws-amplify");
 
 require("dotenv").config();
 
-Amplify.configure(require("./aws-exports"));
+Amplify.configure({
+    aws_project_region: process.env.AWS_REGION,
+    aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQL_ENDPOINT,
+    aws_appsync_region: process.env.AWS_REGION,
+    aws_appsync_authenticationType: process.env.AWS_APPSYNC_AUTHENTICATION_TYPE,
+    aws_appsync_apiKey: process.env.AWS_APPSYNC_API_KEY,
+});
 
 // PORT
 const PORT = 3000 || process.env.PORT;
